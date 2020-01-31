e1c1e7fc5a57a26c118bd9a20e66d35b555fff46
Dante-Morrison <dantemorrison@gmail.com>
2020-01-31 17:59:19 +0000

    added support for castlevania and quake

--- main.py
+++ main.py
@@ -3,11 +3,38 @@ app = Flask('app')
 
 @app.route('/')
 def hello_world():
-    return ("add a /{game name} to the url to play that dos game")
+    return ("""add a /{game name} to the url to play that dos game
+    
+    right now supported games include:
+    doom
+    doom2
+    wolfenstein
+
+    planned for later implementation:
+    castlevania
+    quake
+
+    """)
+
+@app.route('/castlevania')
+def castlevania():
+    return render_template('castlevania.html')
+
+#@app.route('/daggerfall')
+#def daggerfall():
+#    return render_template('daggerfall.html')
 
 @app.route('/doom')
 def doom():
   return render_template('index.html')
+
+@app.route('/doom2')
+def doom2():
+    return render_template('doom2.html')
+
+@app.route('/quake')
+def quake():
+    return render_template('quake.html')
 
 @app.route('/wolfenstein')
 def Wolf3D():
